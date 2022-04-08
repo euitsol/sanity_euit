3637a6866062ac196d1f29e0d5887b64ac10d9ee
euitsol <euitsols.suprt@gmail.com>
2022-04-08 17:02:51 +0600

    course details fetch from sanity client

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
 import Head from 'next/head';
-import { Typography } from '@mui/material';
+import { Alert, CircularProgress, Grid, Typography } from '@mui/material';
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+import client from '../utils/client';
+import ProductItem from '../components/ProductItem';
 
 export default function Home() {
-  return <Layout>List Products</Layout>;
+  const [state, setState] = useState({
+    course_prod: [],
+    error: '',
+    loading: true,
+  });
+  const { loading, error, course_prod } = state;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const course_prod = await client.fetch(`*[_type == "course_product"]`);
+        setState({ course_prod, loading: false });
+      } catch (err) {
+        setState({ loading: false, error: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Layout>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert variant="danger">{error}</Alert>
+      ) : (
+        <Grid container spacing={3}>
+          {course_prod.map((course_product) => (
+            <Grid item md={4} key={course_product.slug}>
+              <ProductItem course_product={course_product}></ProductItem>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Layout>
+  );
 }
