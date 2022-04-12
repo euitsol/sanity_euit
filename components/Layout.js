bfbd977a6f8aabd08d79cbe36ed0a4f1972a7c34
euitsol <euitsols.suprt@gmail.com>
2022-04-12 18:06:55 +0600

    job success and welcom to added

--- components/Layout.js
+++ components/Layout.js
@@ -224,12 +224,87 @@ export default function layout({ title, description, children }) {
         </Box>
       </Container>
 
+      <Container component="main">
+        <Typography variant="h4" textAlign="center">
+          Welcome To
+        </Typography>
+        <Typography variant="h5" textAlign="center">
+          European IT Institute
+        </Typography>
+        <Typography variant="p" textAlign="center">
+          European IT started off as an IT and web-based solutions, services and
+          web design company in Ireland, Germany, and Bangladesh in 2009. Over
+          the years, European IT has delivered successful projects in multiple
+          platforms to clients in the US, Canada, UK, Ireland, Norway, Belgium,
+          Netherlands, Bangladesh, Australia to name a few countries. The
+          founders decided to set up European IT Institute in 2015 to provide
+          professional IT training along with IT Skill Enhancement Training
+          Program with the outsourcing market for graduates in view to gain work
+          that follows international standards and quality. Additionally,
+          European IT Institute offers training delivery solutions to
+          corporations, institutions, and individuals along with internship for
+          graduates.
+        </Typography>
+        <hr></hr>
+      </Container>
+
       <Container component="main" sx={classes.main}>
         <Typography variant="h3" textAlign="center">
           Our Courses
         </Typography>
         <hr></hr>
         {children}
+      </Container>
+      <Container component="main">
+        <Typography variant="h3" textAlign="center">
+          Success
+        </Typography>
+        <Box sx={{ flexGrow: 2 }}>
+          <Grid container>
+            <Grid item xs={6} md={6}>
+              <Item sx={{ background: 'white' }}>
+                {' '}
+                <Card sx={{ maxWidth: 550 }}>
+                  <CardActionArea>
+                    <CardContent sx={{ height: 500 }}>
+                      <Typography gutterBottom variant="h5" component="div">
+                        European IT started off as an IT and web-based
+                        solutions, services and web design company in Ireland,
+                        Germany, and Bangladesh in 2009. Over the years,
+                        European IT has delivered successful projects in
+                        multiple platforms to clients in the US, Canada, UK,
+                        Ireland, Norway, Belgium, Netherlands, Bangladesh,
+                        Australia to name a few countries. The founders decided
+                        to set up European IT Institute in 2015 to provide
+                        professional IT training along with IT Skill Enhancement
+                        Training Program with the outsourcing market for
+                        graduates in view to gain work that follows
+                        international standards and quality. Additionally,
+                        European IT Institute offers training delivery solutions
+                        to corporations, institutions, and individuals along
+                        with internship for graduates.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Item>
+            </Grid>
+            <Grid item xs={6} md={6}>
+              <Item>
+                <Card sx={{ maxWidth: 550 }}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="500px"
+                      image="images/slider1.webp"
+                      alt="green iguana"
+                    />
+                  </CardActionArea>
+                </Card>
+              </Item>
+            </Grid>
+          </Grid>
+        </Box>
       </Container>
       <Box component="footer" sx={classes.footer}>
         <Typography>All right reserved. Sanity Euit</Typography>
