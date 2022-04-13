0f347fd314e073382fc3736419c3f45842b29803
euitsol <euitsols.suprt@gmail.com>
2022-04-13 15:21:07 +0600

    our facilities section added

--- components/Layout.js
+++ components/Layout.js
@@ -30,7 +30,9 @@ import Carousel from 'react-elastic-carousel';
 import Item from './Item';
 import styles from '../styles/carosoul.module.css';
 import { height } from '@mui/system';
+import { SupportAgent } from '@mui/icons-material';
 const breakPoints = [{ width: 1, itemsToShow: 1 }];
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 export default function layout({ title, description, children }) {
   const theme = createTheme({
     components: {
@@ -255,57 +257,144 @@ export default function layout({ title, description, children }) {
         <hr></hr>
         {children}
       </Container>
-      {/* <Container component="main">
-        <Typography variant="h3" textAlign="center">
-          Success
+
+      <Container component="main">
+        <Typography variant="h6" textAlign="center">
+          European IT
         </Typography>
-        <Box sx={{ flexGrow: 2 }}>
-          <Grid container>
-            <Grid item xs={6} md={6}>
+        <Typography variant="h5" textAlign="center">
+          Facilities
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={4} md={4}>
               <Item sx={{ background: 'white' }}>
                 {' '}
-                <Card sx={{ maxWidth: 550 }}>
+                <Card sx={{ maxWidth: 500 }}>
                   <CardActionArea>
-                    <CardContent sx={{ height: 500 }}>
+                    <SupportAgentIcon sx={{ fontSize: 40 }} />
+
+                    <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        European IT started off as an IT and web-based
-                        solutions, services and web design company in Ireland,
-                        Germany, and Bangladesh in 2009. Over the years,
-                        European IT has delivered successful projects in
-                        multiple platforms to clients in the US, Canada, UK,
-                        Ireland, Norway, Belgium, Netherlands, Bangladesh,
-                        Australia to name a few countries. The founders decided
-                        to set up European IT Institute in 2015 to provide
-                        professional IT training along with IT Skill Enhancement
-                        Training Program with the outsourcing market for
-                        graduates in view to gain work that follows
-                        international standards and quality. Additionally,
-                        European IT Institute offers training delivery solutions
-                        to corporations, institutions, and individuals along
-                        with internship for graduates.
+                        24/7 Online Support
+                      </Typography>
+                      <Typography variant="p" color="text.secondary">
+                        Each time is very important to us. European IT is giving
+                        24/7 online support for the students for any information
+                        review and solving problem.
                       </Typography>
                     </CardContent>
                   </CardActionArea>
                 </Card>
               </Item>
             </Grid>
-            <Grid item xs={6} md={6}>
+            <Grid item xs={4} md={4}>
               <Item>
-                <Card sx={{ maxWidth: 550 }}>
+                <Card sx={{ maxWidth: 500 }}>
                   <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="500px"
-                      image="images/slider1.webp"
-                      alt="green iguana"
-                    />
+                    <SupportAgentIcon sx={{ fontSize: 40 }} />
+
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lifetime Support
+                      </Typography>
+                      <Typography variant="p" color="text.secondary">
+                        By the end of the course, European IT provides life time
+                        support to its students for problem solving and
+                        collaborate.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Item>
+            </Grid>
+            <Grid item xs={4} md={4}>
+              <Item>
+                <Card sx={{ maxWidth: 500 }}>
+                  <CardActionArea>
+                    <SupportAgentIcon sx={{ fontSize: 40 }} />
+
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Practice Lab Support
+                      </Typography>
+                      <Typography variant="p" color="text.secondary">
+                        European IT is giving students a practice lab at the end
+                        of classes or at any time to practice. Where students
+                        will have the opportunity to practice for a long time.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Item>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={4} md={4} sx={{ maxHeight: 400 }}>
+              <Item sx={{ background: 'white' }}>
+                {' '}
+                <Card sx={{ maxWidth: 500 }}>
+                  <CardActionArea>
+                    <SupportAgentIcon sx={{ fontSize: 40 }} />
+
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Class Video
+                      </Typography>
+                      <Typography variant="p" color="text.secondary">
+                        Video for each class of European IT Institute is stored
+                        and provided to students. This will allow students to
+                        better understand the contents of the class.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Item>
+            </Grid>
+            <Grid item xs={4} md={4}>
+              <Item>
+                <Card sx={{ maxWidth: 500 }}>
+                  <CardActionArea>
+                    <SupportAgentIcon sx={{ fontSize: 40 }} />
+
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Job Placement
+                      </Typography>
+                      <Typography variant="p" color="text.secondary">
+                        European IT manage job for talented students through
+                        their job placement cell at European IT's own firm as
+                        well as other firms.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Item>
+            </Grid>
+            <Grid item xs={4} md={4}>
+              <Item>
+                <Card sx={{ maxWidth: 500 }}>
+                  <CardActionArea>
+                    <SupportAgentIcon sx={{ fontSize: 40 }} />
+
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Job Placement
+                      </Typography>
+                      <Typography variant="p" color="text.secondary">
+                        European IT manage job for talented students through
+                        their job placement cell at European IT's own firm as
+                        well as other firms.
+                      </Typography>
+                    </CardContent>
                   </CardActionArea>
                 </Card>
               </Item>
             </Grid>
           </Grid>
         </Box>
-      </Container> */}
+      </Container>
+
       <Box component="footer" sx={classes.footer}>
         <Typography>All right reserved. Sanity Euit</Typography>
       </Box>
