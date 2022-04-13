28b73e490fd50b0aebb125df9360d9f318adef0d
euitsol <euitsols.suprt@gmail.com>
2022-04-13 16:19:17 +0600

    our partners added2

--- components/Layout.js
+++ components/Layout.js
@@ -159,7 +159,7 @@ export default function layout({ title, description, children }) {
         </div>
       </Container>
       <Container component="main">
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1 }} className={styles.slide_after}>
           <Grid container spacing={2}>
             <Grid item xs={4} md={4}>
               <Item sx={{ background: 'white' }}>
@@ -240,18 +240,18 @@ export default function layout({ title, description, children }) {
           European IT Institute
         </Typography>
         <Typography variant="p" textAlign="center">
-          European IT started off as an IT and web-based solutions, services and
-          web design company in Ireland, Germany, and Bangladesh in 2009. Over
-          the years, European IT has delivered successful projects in multiple
-          platforms to clients in the US, Canada, UK, Ireland, Norway, Belgium,
-          Netherlands, Bangladesh, Australia to name a few countries. The
-          founders decided to set up European IT Institute in 2015 to provide
-          professional IT training along with IT Skill Enhancement Training
-          Program with the outsourcing market for graduates in view to gain work
-          that follows international standards and quality. Additionally,
-          European IT Institute offers training delivery solutions to
-          corporations, institutions, and individuals along with internship for
-          graduates.
+          European IT startedasdasdsad off as an IT and web-based solutions,
+          services and web design company in Ireland, Germany, and Bangladesh in
+          2009. Over the years, European IT has delivered successful projects in
+          multiple platforms to clients in the US, Canada, UK, Ireland, Norway,
+          Belgium, Netherlands, Bangladesh, Australia to name a few countries.
+          The founders decided to set up European IT Institute in 2015 to
+          provide professional IT training along with IT Skill Enhancement
+          Training Program with the outsourcing market for graduates in view to
+          gain work that follows international standards and quality.
+          Additionally, European IT Institute offers training delivery solutions
+          to corporations, institutions, and individuals along with internship
+          for graduates.
         </Typography>
         <hr></hr>
       </Container>
@@ -369,8 +369,8 @@ export default function layout({ title, description, children }) {
                       </Typography>
                       <Typography variant="p" color="text.secondary">
                         European IT manage job for talented students through
-                        their job placement cell at European IT's own firm as
-                        well as other firms.
+                        their job placement cell at European IT own firm as well
+                        as other firms.
                       </Typography>
                     </CardContent>
                   </CardActionArea>
@@ -389,8 +389,8 @@ export default function layout({ title, description, children }) {
                       </Typography>
                       <Typography variant="p" color="text.secondary">
                         European IT manage job for talented students through
-                        their job placement cell at European IT's own firm as
-                        well as other firms.
+                        their job placement cell at European IT own firm as well
+                        as other firms.
                       </Typography>
                     </CardContent>
                   </CardActionArea>
